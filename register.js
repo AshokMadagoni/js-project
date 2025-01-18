239c2849ba6ee29a4c5062f21b78cd592e73ae6b
AshokMadagoni <madagoniashok1@gmail.com>
2025-01-18 22:43:11 +0530

    updated with hidden password

--- register.js
+++ register.js
@@ -21,3 +21,18 @@ function addUser() {
         alert("Please enter both username and password.");
     }
 }
+
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+    const toggleIcon = document.getElementById("toggle-password");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        toggleIcon.classList.remove("fa-eye");
+        toggleIcon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        toggleIcon.classList.remove("fa-eye-slash");
+        toggleIcon.classList.add("fa-eye");
+    }
+}
